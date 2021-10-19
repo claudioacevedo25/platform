0286dc1f71dc1bddd66b75e397cf1048f73fd7a5
Maxi <maximiliano.pezzotta@splight-ae.com>
2021-10-19 16:50:18 -0300

    ft add auth0 integrations

---
+++ pages/api/auth/getSession.js
@@ -0,0 +1,13 @@
+import { getAccessToken, withApiAuthRequired } from '@auth0/nextjs-auth0';
+
+export default withApiAuthRequired(async function products(req, res) {
+	try {
+		const { accessToken } = await getAccessToken(req, res);		
+		res.json({
+			accessToken,
+		});
+	} catch(error) {
+		res.status(error.status || 500).end(error.message);
+	}
+});
+
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,26 @@
 import { injectIntl, FormattedMessage } from 'react-intl';
+import { useUser } from '@auth0/nextjs-auth0';
 import messages from '../components/index.messages';
 
+
 const Home = () => {
-	return (
-		<div >
-			<FormattedMessage {...messages.home} />
-		</div>
-	);
+
+	const { user, error, isLoading } = useUser();
+	if (isLoading) return <div>Loading...</div>;
+	if (error) return <div>{error.message}</div>;
+	if (user) {
+		return (  
+			<div>
+				<div>
+					<h1>
+						<FormattedMessage {...messages.home}/>
+					</h1>
+				</div>
+			Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
+			</div>
+		);
+	}
+	return <a href="/api/auth/login">Login</a>;
 };
 
 export default injectIntl(Home);

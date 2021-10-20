4c70f8c00201d1681bca3769ffae4844f7a5cb5c
Maxi <maximiliano.pezzotta@splight-ae.com>
2021-10-20 17:11:38 -0300

    get user session from auth0

--- pages/api/auth/getSession.js
+++ pages/api/auth/getSession.js
@@ -1,11 +1,11 @@
-import { getAccessToken, withApiAuthRequired } from '@auth0/nextjs-auth0';
+import { withApiAuthRequired, getSession } from '@auth0/nextjs-auth0';
 
 export default withApiAuthRequired(async function products(req, res) {
 	try {
-		const { accessToken } = await getAccessToken(req, res);	
-		//call to the external API and return the result in res.json	
+		const session  = await getSession(req, res);	
+		//call to the external API and return the result in res.json		
 		res.json({
-			accessToken,
+			session,
 		});
 	} catch(error) {
 		res.status(error.status || 500).end(error.message);

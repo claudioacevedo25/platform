7288ee46a072e0adf72f3566f41ab78052f43e63
Maxi <maximiliano.pezzotta@splight-ae.com>
2021-10-20 12:07:15 -0300

    add audience to auth0

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,2 +1,17 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
-export default handleAuth();
+import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
+
+export default handleAuth({
+	async login(req, res) {
+		try {
+			await handleLogin(req, res, {
+				authorizationParams: {
+					audience: 'https://splight-ae.com/test', // or AUTH0_AUDIENCE
+					// Add the `offline_access` scope to also get a Refresh Token
+					scope: 'openid profile email read:assets' // or AUTH0_SCOPE
+				}
+			});
+		} catch (error) {
+			res.status(error.status || 400).end(error.message);
+		}
+	}
+});
--- pages/api/auth/getSession.js
+++ pages/api/auth/getSession.js
@@ -2,7 +2,8 @@ import { getAccessToken, withApiAuthRequired } from '@auth0/nextjs-auth0';
 
 export default withApiAuthRequired(async function products(req, res) {
 	try {
-		const { accessToken } = await getAccessToken(req, res);		
+		const { accessToken } = await getAccessToken(req, res);	
+		//call to the external API and return the result in res.json	
 		res.json({
 			accessToken,
 		});
@@ -10,4 +11,3 @@ export default withApiAuthRequired(async function products(req, res) {
 		res.status(error.status || 500).end(error.message);
 	}
 });
-

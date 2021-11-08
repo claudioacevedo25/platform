c81867afecb54f409d1f9e25058a67e57cbb5be2
Maxi <maximiliano.pezzotta@splight-ae.com>
2021-11-08 13:49:02 -0300

    fac-68-header: ft add header to platform

---
+++ components/DefaultLayout/defaultLayout.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { useUser } from '@auth0/nextjs-auth0';
+import { Header, Avatar, Popover, Typography, Button } from '@splight-ae/splight-ui';
+import { pages } from '../../constants/pages';
+import style from './defaultLayout.module.css';
+
+const DefaultLayout = ({ children }) => {
+	const { user } = useUser();
+
+	const avatar =
+		<Popover position="right" displayLabel={<Avatar src={user && user.picture} name={user && user.given_name} />}>
+			<Avatar
+				src={user && user.picture}
+				name={user && user.name}
+			/>
+			<Typography variant="h3">{user && user.given_name}</Typography>
+			<Button>My Profile</Button>
+		</Popover>;
+
+	const [currentPage, setCurrentPage] = useState(pages[0]);
+	const changePage = (page) => {
+		setCurrentPage(page);
+	};
+	return (
+		<>
+			{
+				!!user && <Header pages={pages} currentPage={currentPage} onClick={changePage} user={avatar} />
+			}
+
+			<main className= {style.defaultLayout__main}>{children}</main>
+		</>
+	);
+};
+
+export default DefaultLayout;
--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,22 @@
 import { injectIntl, FormattedMessage } from 'react-intl';
-import { Header } from '@splight-ae/splight-ui';
 import { useUser } from '@auth0/nextjs-auth0';
 import messages from '../components/index.messages';
 
 const Login = () => {
-	const pages = [
-		{ name: "home", id: 1, url: "/home" },
-		{ name: "digital solution", id: 2, url: "/digital-solution" },
-		{ name: "artificial energy", id: 3, url: "/artificial-energy" },
-	];
-	const [currentPage, setCurrentPage] = useState(pages[0]);
-	const changePage = (page) => {
-		setCurrentPage(page);
-	};
 
-// esta pagina va ser la que te redireccione a auth0, va ser nuestro "login"
+	// esta pagina va ser la que te redireccione a auth0, va ser nuestro "login"
 	const { user, error, isLoading } = useUser();
 	if (isLoading) return <div>Loading...</div>;
 	if (error) return <div>{error.message}</div>;
 	if (user) {
-		return (  
+		return (
 			<div>
-				<Header pages={pages} currentPage={currentPage} onClick={changePage} />
 				<div>
 					<h1>
-						<FormattedMessage {...messages.home}/>
+						<FormattedMessage {...messages.home} />
 					</h1>
 				</div>
-			Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
+				Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
 			</div>
 		);
 	}

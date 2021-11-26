cc55ff8b1bf1b3f0a4b0e2693c04c9d62ec229a5
Maxi <maximiliano.pezzotta@splight-ae.com>
2021-11-26 10:45:36 -0300

    fac-103: change padding top on home page

--- components/HomePage/homePage.component.jsx
+++ components/HomePage/homePage.component.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { injectIntl, FormattedMessage } from 'react-intl';
+import { useRouter } from 'next/router';
 import { SplightRoundedIcon, Typography, BasicCard, Tabs, Tab } from '@splight-ae/splight-ui';
 import messages from './homePage.messages';
 import style from './homePage.module.css';
 
 const HomePageComponent = ({ digitalOffer, fetchFavs, fetchRecents }) => {
+	const router = useRouter();
+
+	const onDigitalOffer = (id) => {
+		// router.push(`digital-solution/${id}`);
+	};
 	return (
 		<div className={style.homePage__container}>
 			<div className={style.homePage__welcome}>
@@ -22,17 +28,30 @@ const HomePageComponent = ({ digitalOffer, fetchFavs, fetchRecents }) => {
 				<Tabs width='80'>
 					<Tab onClick={fetchFavs} className={style.homePage__content__tab} label='fav' tabName='Fav' height='50'>
 						{
-							!digitalOffer.fav.length ? <BasicCard title={'There are not Favs'} subtitle='No Actions' width='250' height='137'/> :
+							!digitalOffer.fav.length ? <BasicCard title={'There are not Favs'} subtitle='No Actions' width='250' height='137' /> :
 								digitalOffer.fav.map((favorite, index) =>
-									<BasicCard key={index} title={favorite.title} subtitle='Go Now' width='250' height='137' />
+									<BasicCard
+										onClick={() => onDigitalOffer(favorite.digital_offer.id)}
+										key={index} title={favorite.digital_offer.name}
+										subtitle='Go Now'
+										width='250'
+										height='137'
+									/>
 								)
 						}
 					</Tab>
 					<Tab onClick={fetchRecents} className={style.homePage__content__tab} label='recents' tabName='Recents' height='50'>
 						{
-							!digitalOffer.recents.length ? <BasicCard title={'There are not Recents'} subtitle='No Actions' width='250' height='137'/> :
+							!digitalOffer.recents.length ? <BasicCard title={'There are not Recents'} subtitle='No Actions' width='250' height='137' /> :
 								digitalOffer.recents.map((recent, index) =>
-									<BasicCard key={index} title={recent.title} subtitle='Go Now' width='250' height='137' />
+									<BasicCard
+										onClick={() => onDigitalOffer(recent.digital_offer.id)}
+										key={index}
+										title={recent.digital_offer.name}
+										subtitle='Go Now'
+										width='250'
+										height='137'
+									/>
 								)
 						}
 					</Tab>
@@ -46,7 +65,7 @@ const HomePageComponent = ({ digitalOffer, fetchFavs, fetchRecents }) => {
 HomePageComponent.defaultProps = {
 	digitalOffer: {},
 	fetchFavs: () => { },
-	fetchRecents: () => {},
+	fetchRecents: () => { },
 };
 
 HomePageComponent.propTypes = {
